2bdae81a8005bc5e3cb20f5aaccd6a1a102c4991
behzad <>
2020-12-14 01:40:56 +0330

    Add ButtonPress

--- Assets/Scripts/ButtonPress.cs
+++ Assets/Scripts/ButtonPress.cs
@@ -1,18 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class ButtonPress : MonoBehaviour
 {
     public Vector2 press = new Vector2(.1f, .1f);
-    Vector2 scale = new Vector2(1, 1);
+    Vector2 scale;
+    float expectedScale;
 
-    void Update()
+    private void Start()
     {
-        scale.x += (1 - scale.x) / 5;
-        scale.y += (1 - scale.y) / 5;
-        GetComponent<RectTransform>().localScale = new Vector2(scale.x, scale.y);
-        transform.localScale = new Vector2(scale.x, scale.y);
+        scale = new Vector2(GetComponent<RectTransform>().localScale.x, GetComponent<RectTransform>().localScale.y);
+        expectedScale = GetComponent<RectTransform>().localScale.x;
     }
 
     void OnMouseDown()
@@ -21,4 +21,35 @@ public class ButtonPress : MonoBehaviour
         scale.y -= press.y;
         FindObjectOfType<AudioManager>().Play(Audio.Clip.Click, 1, 0);
     }
+
+    //void Update()
+    //{
+    //    scale.x += (expectedScale - scale.x) / 5;
+    //    scale.y += (expectedScale - scale.y) / 5;
+    //    GetComponent<RectTransform>().localScale = new Vector2(scale.x, scale.y);
+    //    transform.localScale = new Vector2(scale.x, scale.y);
+
+
+    //    //if (Input.GetMouseButtonDown(0))
+    //    //{
+    //    //    RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+
+    //    //    if (hit.collider == GetComponent<BoxCollider2D>())
+    //    //    {
+    //    //        scale.x -= press.x;
+    //    //        scale.y -= press.y;
+
+    //    //    }
+    //    //}
+
+    //    //if (UnityEngine.EventSystems.EventSystem.current.IsPointerOverGameObject(-1))
+    //    //{
+    //    //    if (Input.GetMouseButtonDown(0))
+    //    //    {
+    //    //        scale.x -= press.x;
+    //    //        scale.y -= press.y;
+    //    //        FindObjectOfType<AudioManager>().Play(Audio.Clip.Click, 1, 0);
+    //    //    }
+    //    //}
+
 }
