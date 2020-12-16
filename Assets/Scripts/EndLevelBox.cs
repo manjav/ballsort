873450cfaebba8cfd56033dbe64a0c506f3a12fe
Mansour <mansurjavadi@gmail.com>
2020-12-16 17:03:27 +0330

    Add EndLevelBox

---
+++ Assets/Scripts/EndLevelBox.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class EndLevelBox : MonoBehaviour
+{
+    [SerializeField] private Text coinText;
+
+    void Start()
+    {
+        coinText.text = "x " + GameManager.currentLevel.prize;
+
+        GameManager.player.lastLevel++;
+        GameManager.LoadLevel();
+    }
+}
