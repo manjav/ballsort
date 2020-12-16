5048058b23df81bdabff1b9be999bde7eac69089
Mansour <mansurjavadi@gmail.com>
2020-12-16 16:51:27 +0330

    Add Player prefs

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class Player : PlayerPrefs
+{
+
+    private int _lastLevel;
+    public int lastLevel
+    {
+        get
+        {
+            if (_lastLevel > 0)
+                return _lastLevel;
+            if (HasKey("lastLevel"))
+                _lastLevel = GetInt("lastLevel");
+            else
+                lastLevel = 1;
+            return _lastLevel;
+        }
+        set
+        {
+            if (_lastLevel == value)
+                return;
+            _lastLevel = value;
+            SetInt("lastLevel", _lastLevel);
+            Save();
+        }
+    }
+}
