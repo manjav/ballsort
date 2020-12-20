61084bd23d1687c8083a2cca407b459510e34ffc
Mansour <mansurjavadi@gmail.com>
2020-12-20 17:07:16 +0330

    Move levelText to UI

---
+++ Assets/LevelUI.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelUI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private Text levelText;
+    void Start()
+    {
+        levelText.text = "level " + GameManager.Instance.currentLevel.index.ToString();
+    }
+
+
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,22 +1,19 @@
-﻿using UnityEngine;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     static public Level currentLevel;
     static public Player player;
-    static public Text levelText;
     static public bool gameEnd;
 
-    private void Start()
+    void Start()
     {
         player = new Player();
-        gameEnd = false;
     }
 
     static public void LoadLevel()
     {
         // load xml player.lastLevel
-        levelText.text = "level " + currentLevel.index.ToString();
     }
 }
