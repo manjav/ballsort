667e4f16e625e3ab91165cdf438fa0690ec382aa
behzad <>
2020-12-21 16:23:19 +0330

    Add ShowLevelBox (code, sprite)

---
+++ Assets/Scripts/LevelBox.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class LevelBox : MonoBehaviour
+{
+    public void selectLevel( int levelIndex )
+    {
+        GameManager.Instance.player.lastLevel = levelIndex;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
--- Assets/Scripts/LevelUI.cs
+++ Assets/Scripts/LevelUI.cs
@@ -1,13 +1,29 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class LevelUI : MonoBehaviour
 {
     // Start is called before the first frame update
     [SerializeField] private Text levelText;
+    [SerializeField] private GameObject showLevelsBox;
+
 
     internal void Refresh()
     {
-        levelText.text = "Level " + (GameManager.Instance.currentLevel.index + 1).ToString();
+        levelText.text = "Level " + (GameManager.Instance.currentLevel.index+1).ToString();
+    }
+
+    public void ReloadLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void ShowLevelsBox()
+    {
+        if (showLevelsBox.activeInHierarchy == false)
+            showLevelsBox.gameObject.SetActive(true);
+        else
+            showLevelsBox.gameObject.SetActive(false);
     }
 }
