e077bcda1feeecae6840f637937557cf7a3727b8
behzad <>
2020-12-22 03:14:24 +0330

    Select levels from levels list (1-10)

--- Assets/Scripts/LevelBox.cs
+++ Assets/Scripts/LevelBox.cs
@@ -2,12 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LevelBox : MonoBehaviour
 {
-    public void selectLevel( int levelIndex )
+    public int levelIndex;
+    public Text text;
+
+    void Start()
     {
-        GameManager.Instance.player.lastLevel = levelIndex;
+        text.text = levelIndex.ToString();
+    }
+
+    public void selectLevel()
+    {
+        GameManager.Instance.player.lastLevel = levelIndex-1;
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        gameObject.SetActive(false);
     }
 }
